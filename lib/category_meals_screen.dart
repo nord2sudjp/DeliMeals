a7dc1c9aff420355739f759fb653708bd712fcf3
maka y <nord2sudjp@gmail.com>
2020-11-16 17:51:00 +0900

    Show meals in the give category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -13,17 +14,24 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final title = routeArgs['title'];
     final id = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: 
-            // Center(
-      //   child: Text('The Recipies for The Category'),
-      // )
-      ListView.builder(itemBuilder:(ctx, ))
-      ,
+      body:
+          // Center(
+          //   child: Text('The Recipies for The Category'),
+          // )
+          ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
