8943b293bab652259668005ce2988c7859fa0d20
maka y <nord2sudjp@gmail.com>
2020-11-16 16:48:07 +0900

    Navigator from item to Meals

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,3 +1,4 @@
+import 'package:DeliMeals/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -6,17 +7,31 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    print("SelectCategory");
+    Navigator.of(ctx).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoryMealsScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.headline6),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [color.withOpacity(0.7), color],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-        borderRadius: BorderRadius.circular(15),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title, style: Theme.of(context).textTheme.headline6),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
