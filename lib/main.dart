d49b547caddac0697eb24b739cfc9efc61207a60
maka y <nord2sudjp@gmail.com>
2020-11-16 16:30:52 +0900

    Completd CateriesScreen

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 255, 229, 1),
+        fontFamily: 'Railway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
