1523dcf789770f0ce3e237f5d41e3a6c1a55eeef
maka y <nord2sudjp@gmail.com>
2020-11-16 15:00:00 +0900

    Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("DeliMeals"),
+        title: Text('DeliMeals'),
       ),
       body: Center(
         child: Text('Navigation Time!'),
