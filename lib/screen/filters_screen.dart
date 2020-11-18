f731ad609c0483076a9d4a4a4933f2ad23257a00
maka y <nord2sudjp@gmail.com>
2020-11-18 22:03:49 +0900

    Implement glutenfree filter

--- lib/screen/filters_screen.dart
+++ lib/screen/filters_screen.dart
@@ -1,8 +1,19 @@
 import '../widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +22,26 @@ class FiltersScreen extends StatelessWidget {
       ),
       drawer: MainDrawer(),
       body: Center(
-        child: Text('Filter'),
+        child: Column(children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text('Adjust your meal selection.',
+                style: Theme.of(context).textTheme.headline6),
+          ),
+          Expanded(
+            child: ListView(children: <Widget>[
+              SwitchListTile(
+                  title: Text('Gluten-free'),
+                  value: _glutenFree,
+                  subtitle: Text("Only include gluten-free meals."),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  }),
+            ]),
+          ),
+        ]),
       ),
     );
   }
