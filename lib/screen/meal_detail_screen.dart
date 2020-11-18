2cb158d05a97039c1f0611fabe477e38928e7fc8
maka y <nord2sudjp@gmail.com>
2020-11-18 13:38:19 +0900

    refactoring buildContainer

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -11,6 +11,19 @@ class MealDetailScreen extends StatelessWidget {
     );
   }
 
+  Widget buildContainer(Widget child) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(15)),
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        height: 150,
+        width: 300,
+        child: child);
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
@@ -31,16 +44,8 @@ class MealDetailScreen extends StatelessWidget {
             ),
           ),
           buildSectionTitle(context, "Ingredients"),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(15)),
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            height: 150,
-            width: 300,
-            child: ListView.builder(
+          buildContainer(
+            ListView.builder(
               itemBuilder: (ctx, index) => Card(
                 color: Theme.of(context).accentColor,
                 child: Padding(
