d3d132d4dd5cd0ed13a5ad5228fe9febc569f97e
maka y <nord2sudjp@gmail.com>
2020-11-18 13:35:50 +0900

    refactoring buildSectionTitle

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -3,6 +3,14 @@ import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = './meal-detail-screen';
+
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(text, style: Theme.of(context).textTheme.headline6),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
@@ -22,11 +30,7 @@ class MealDetailScreen extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text('Ingredients',
-                style: Theme.of(context).textTheme.headline6),
-          ),
+          buildSectionTitle(context, "Ingredients"),
           Container(
             decoration: BoxDecoration(
                 color: Colors.white,
@@ -46,6 +50,7 @@ class MealDetailScreen extends StatelessWidget {
               itemCount: selectedMeal.ingredients.length,
             ),
           ),
+          buildSectionTitle(context, "Steps"),
         ],
       ),
     );
