7f3c8df894e8408032764494dabaecf2cfd322ae
maka y <nord2sudjp@gmail.com>
2020-11-18 10:18:19 +0900

    Show text on the image

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -42,6 +42,15 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.white,
+                  ),
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                )
               ]),
             ],
           )),
