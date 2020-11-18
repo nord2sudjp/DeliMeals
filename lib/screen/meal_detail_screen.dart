11027fc0f62439e4c8394ceb82773a8fa89f9404
maka y <nord2sudjp@gmail.com>
2020-11-18 13:19:27 +0900

    show image in meal detail

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = './meal-detail-screen';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
         title: Text('$mealId'),
       ),
-      body: Center(
-        child: Text('The meal : $mealId!'),
-      ),
+      body: Column(children: <Widget>[
+        Container(
+          height: 300,
+          width: double.infinity,
+          child: Image.network(
+            selectedMeal.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ]),
     );
   }
 }
